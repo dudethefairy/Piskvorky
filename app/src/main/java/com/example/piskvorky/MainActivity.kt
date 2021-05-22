ece5bee79f7c72abe9f640797277ff878a73372c
dudethefairy <t.zlatohlavek@gmail.com>
2021-05-22 16:53:19 +0200

    test commit

--- app/src/main/java/com/example/piskvorky/MainActivity.kt
+++ app/src/main/java/com/example/piskvorky/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.piskvorky
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     //Testovací commit
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        butt_start.setOnClickListener {clickStartListener()}
+    }
+
+    private val butt_start by lazy {
+        findViewById<Button>(R.id.butt_start)
+    }
+
+    public fun clickStartListener(){
+
+
     }
 
     fun ahoj(){
