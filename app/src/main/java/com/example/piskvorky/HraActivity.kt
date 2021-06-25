b6c0b6abdac7a1c872c568912319887e9ca7d688
Filda360 <Filda360@seznam.cz>
2021-06-25 19:32:14 +0200

    commit Filip

--- app/src/main/java/com/example/piskvorky/HraActivity.kt
+++ app/src/main/java/com/example/piskvorky/HraActivity.kt
@@ -9,23 +9,48 @@ import android.widget.ImageButton
 import android.widget.LinearLayout
 
 class HraActivity : AppCompatActivity() {
+
+    val radky : Int = 10
+    val sloupce : Int = 7
+    val pole = Array(radky, {IntArray(sloupce)})
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hra)
 
-        val button = Button(this)
-        // setting layout_width and layout_height using layout parameter
-        button.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT)
 
-        val button1 = ImageButton(this)
-        // setting layout_width and layout_height using layout parameter
-        button1.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT)
 
         val layout = findViewById(R.id.gridLayout) as GridLayout
+
         // add Button to LinearLayout
-        layout.addView(button)
-        layout.addView(button1)
+
+        //vykreslení hrací plochy (tlačítek)
+        for (i in 0..9) {
+            for (j in 0..6) {
+
+                val button1 = ImageButton(this)
+                button1.setImageDrawable(getResources().getDrawable(R.drawable.prazdne))
+                // setting layout_width and layout_height using layout parameter
+                button1.layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+                button1.setOnClickListener{
+                    pole[i][j] = 5
+                    System.out.println(pole[i][j])
+                    System.out.println("radek " + i)
+                    System.out.println("sloupec " + j)
+                }
+
+                layout.addView(button1)
+
+            }
+        }
+
+        for (row in pole) {
+            println(row?.contentToString())
+        }
     }
+
+
 }
